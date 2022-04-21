628700c9e15e9317bcabec65f26e1d25a0c3b57e
Emil <emilsuuronen@gmail.com>
2022-04-21 10:24:27 +0300

    Projekti

--- cocktailSearch.js
+++ cocktailSearch.js
@@ -6,7 +6,7 @@ lomake.addEventListener('submit', function (evt) {
   evt.preventDefault();
   const search = document.querySelector('input[name=searchBox]').value;
 
-  fetch('https://api.tvmaze.com/search/shows?q=' + search).then(function (result) {
+  fetch('www.thecocktaildb.com/api/json/v1/1/search.php?s="' + search).then(function (result) {
     return result.json();
   }).then(function (series) {
     showSeries(series);

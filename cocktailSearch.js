e79208bcc06d4b3f004a299c6becf9eceb4eddfc
Emil <emilsuuronen@gmail.com>
2022-04-24 22:11:49 +0300

    CSS and cocktailSerach.js updates

--- cocktailSearch.js
+++ cocktailSearch.js
@@ -1,35 +1,43 @@
 'use strict'
 
+getData();
 
-fetch('https://thecocktaildb.com/api/json/v1/1/random.php').then(function (result) {
-  return result.json();
-  console.log(result);
-}).then(function (drinks) {
-  displayCocktail(drinks);
-}).catch(function (error) {
-  console.log(error);
-});
+const randomDrinkBtn = document.getElementById("newDrink");
 
-function displayCocktail(drinks){
+randomDrinkBtn.addEventListener("click", getData);
 
-  console.log(drinks);
-  const drinkName = drinks.drinks[0].strDrink;
-  console.log(drinkName);
+function getData() {
 
-  const main = document.querySelector('main');
+  fetch('https://thecocktaildb.com/api/json/v1/1/random.php').then(function (result) {
+    return result.json();
+    console.log(result);
+  }).then(function (drinks) {
+    displayCocktail(drinks);
+  }).catch(function (error) {
+    console.log(error);
+  });
 
-  const drinkOTD = document.getElementById('drinkOTD')
-  main.appendChild(drinkOTD);
+  function displayCocktail(drinks) {
 
-  const cocktailName = document.createElement('H2');
-  cocktailName.textContent = drinks.drinks[0].strDrink;
-  drinkOTD.appendChild(cocktailName);
+    console.log(drinks);
+    const drinkName = drinks.drinks[0].strDrink;
+    console.log(drinkName);
 
-  const drinkImg = document.createElement('img');
-  drinkImg.src = drinks.drinks[0].strDrinkThumb;
-  drinkImg.className = "drinkOTDImg";
-  console.log(drinks.drinks[0].strDrinkThumb);
-  drinkImg.alt = drinks.drinks[0].strDrink;
-  drinkOTD.appendChild(drinkImg);
+    const main = document.querySelector('main');
 
+    const drinkOTD = document.getElementById('drinkOTD')
+    main.appendChild(drinkOTD);
+
+    const cocktailName = document.createElement('H2');
+    cocktailName.textContent = drinks.drinks[0].strDrink;
+    drinkOTD.appendChild(cocktailName);
+
+    const drinkImg = document.createElement('img');
+    drinkImg.src = drinks.drinks[0].strDrinkThumb;
+    drinkImg.className = "drinkOTDImg";
+    console.log(drinks.drinks[0].strDrinkThumb);
+    drinkImg.alt = drinks.drinks[0].strDrink;
+    drinkOTD.appendChild(drinkImg);
+
+  }
 }
